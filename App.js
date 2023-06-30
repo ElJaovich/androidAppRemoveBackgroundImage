03bf2d3d3cb0da44906a0a7c725fe8fa13d74e60
ElJaovich <eliasjaovich@gmail.com>
2023-06-30 00:36:22 -0400

    add expo-image-picker

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Image, Text, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function App() {
+export default function ImagePickerExample() {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Removedor de fondos de Imagenes</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && (
+        <View style={{ marginTop: 20, alignItems: 'center' }}>
+          <Image source={{ uri: image.assets[0].uri }} style={{ width: 200, height: 200 }} />
+          <Text>URI: {image.assets[0].uri}</Text>
+          <Text>Width: {image.assets[0].width}</Text>
+          <Text>Height: {image.assets[0].height}</Text>
+          <Text>Type: {image.assets[0].type}</Text>
+        </View>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
